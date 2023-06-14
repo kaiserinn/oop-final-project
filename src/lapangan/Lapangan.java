ea815b675547bf9331d029390dd75c549b4a1d0d
Azhar Rizqullah <azharizqullah90@gmail.com>
2023-06-15 03:00:34 +0800

    added lapangan table functionality

---
+++ src/lapangan/Lapangan.java
@@ -0,0 +1,54 @@
+package lapangan;
+
+public class Lapangan {
+    private int id;
+    private String nama;
+    private String harga;
+    private String status;
+
+    public Lapangan() {
+        this.id = 0;
+        this.nama = "";
+        this.harga = "";
+        this.status = "";
+    }
+
+    public Lapangan(int id, String nama, String harga, String status) {
+        this.id = id;
+        this.nama = nama;
+        this.harga = harga;
+        this.status = status;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getHarga() {
+        return this.harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
