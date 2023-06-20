233d8fbbc06784c548054174dbd9647b6576584e
bramatama <bramatamar@gmail.com>
2023-06-21 02:30:28 +0800

    added support for payment system

--- src/com/kelompok4/types/User.java
+++ src/com/kelompok4/types/User.java
@@ -8,18 +8,25 @@ import com.kelompok4.DB;
 
 public class User extends Account {
     private String role;
+    private String email;
+    private int balance;
 
     public User(String username, String password) {
         super(0, username, password);
+        this.balance = 0;
+        this.role = "user";
     }
 
-    public User(int id, String username, String password) {
+    public User(int id, String username, String password, int balance) {
         super(id, username, password);
+        this.balance = balance;
+        this.role = "user";
     }
 
-    public User(int id, String username, String password, String role) {
+    public User(int id, String username, String password, String role, int balance) {
         super(id, username, password);
         this.role = role;
+        this.balance = balance;
     }
 
     public int getDatabaseId() {
@@ -49,6 +56,58 @@ public class User extends Account {
         } return -1;
     }
 
+    public int getDatabaseBalance() {
+        try {
+            DB.loadJDBCDriver();
+            DB.connect();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            PreparedStatement statement = DB.prepareStatement("SELECT balance FROM akun WHERE username = ? AND password = ?");
+            statement.setString(1, this.getUsername());
+            statement.setString(2, this.getPassword());
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt("balance");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                DB.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } return -1;
+    }
+
+    public void setDatabaseBalance(int balance, int id) {
+        try {
+            DB.loadJDBCDriver();
+            DB.connect();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            PreparedStatement statement = DB.prepareStatement("UPDATE akun SET balance = ? WHERE id = ?");
+            statement.setInt(1, balance);
+            statement.setInt(2, id);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                DB.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
     public boolean tambahAkun() {
         try {
             DB.loadJDBCDriver();
@@ -67,10 +126,11 @@ public class User extends Account {
                 return false;
             }
 
-            PreparedStatement statement = DB.prepareStatement("INSERT INTO akun (username, password, role) VALUES (?, ?, ?)");
+            PreparedStatement statement = DB.prepareStatement("INSERT INTO akun (username, password, role, balance) VALUES (?, ?, ?, ?)");
             statement.setString(1, this.getUsername());
             statement.setString(2, this.getPassword());
             statement.setString(3, "user");
+            statement.setInt(4, this.balance);
             statement.executeUpdate();
             return true;
         } catch (Exception e) {
@@ -93,10 +153,11 @@ public class User extends Account {
         }
 
         try {
-            PreparedStatement statement = DB.prepareStatement("UPDATE akun SET username = ?, password = ? WHERE id = ?");
+            PreparedStatement statement = DB.prepareStatement("UPDATE akun SET username = ?, password = ?, balance = ? WHERE id = ?");
             statement.setString(1, this.getUsername());
             statement.setString(2, this.getPassword());
-            statement.setInt(3, id);
+            statement.setInt(3, this.balance);
+            statement.setInt(4, id);
             statement.executeUpdate();
             return true;
         } catch (Exception e) {
@@ -168,4 +229,25 @@ public class User extends Account {
     public String getRole() {
         return role;
     }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
 }
