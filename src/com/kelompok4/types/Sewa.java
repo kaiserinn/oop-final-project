783443ee648874d88e0e80ba0523c8c52c182631
bramatama <bramatamar@gmail.com>
2023-06-21 02:28:50 +0800

    updated crud compatibility

--- src/com/kelompok4/types/Sewa.java
+++ src/com/kelompok4/types/Sewa.java
@@ -1,6 +1,7 @@
 package com.kelompok4.types;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Date;
 import java.time.LocalDate;
@@ -11,15 +12,43 @@ public class Sewa {
     private int id;
     private LocalDate tanggal;
     private String sesi;
-    private int lapanganId;
     private int userId;
+    private int lapanganId;
 
-    public Sewa(int id, LocalDate tanggal, String sesi, int lapanganId, int userId) {
+    public Sewa(int id, LocalDate tanggal, String sesi, int userId, int lapanganId) {
         this.id = id;
         this.tanggal = tanggal;
         this.sesi = sesi;
-        this.lapanganId = lapanganId;
         this.userId = userId;
+        this.lapanganId = lapanganId;
+    }
+
+    public int getId() {
+        try {
+            DB.loadJDBCDriver();
+            DB.connect();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            PreparedStatement statement = DB.prepareStatement("SELECT id FROM sewa WHERE tanggal = ? AND sesi = ? AND id_lapangan = ?");
+            statement.setDate(1, Date.valueOf(this.tanggal));
+            statement.setString(2, this.sesi);
+            statement.setInt(3, this.lapanganId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt("id");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                DB.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } return -1;
     }
 
     public boolean sewaLapangan() {
@@ -31,6 +60,7 @@ public class Sewa {
         }
 
         try {
+            System.out.println(this.tanggal + " " + this.sesi + " " + this.userId + " " + this.lapanganId);
             PreparedStatement statement = DB.prepareStatement("INSERT INTO sewa (tanggal, sesi, id_user, id_lapangan) VALUES (?, ?, ?, ?)");
             statement.setDate(1, Date.valueOf(this.tanggal));
             statement.setString(2, this.sesi);
@@ -51,12 +81,27 @@ public class Sewa {
         return false;
     }
 
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public boolean hapus(int id) {
+        try {
+            DB.loadJDBCDriver();
+            DB.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            PreparedStatement stm = DB.prepareStatement("DELETE FROM sewa WHERE id = ?");
+            stm.setInt(1, id);
+            stm.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                DB.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } return false;
     }
 
     public LocalDate getTanggal() {
